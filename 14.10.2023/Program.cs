c077f15f021291087482df839b94342faa337746
HuseynliTogrul <toghrulih@code.edu.az>
2023-10-14 17:26:12 +0400

    student deyerlerin daxil etdik

---
+++ 14.10.2023/Program.cs
@@ -0,0 +1,11 @@
+﻿Student student = new Student();
+student.Name = "Togrul";
+student.SurName = "Huseynli";
+student.GetName(student.Name, student.SurName);
+
+
+student.Group = 655.19;
+student.Point = 70;
+student.isGraduated = true;
+student.GetValue(student.Group, student.Point, student.isGraduated);
+student.EnterExam();
---
+++ 14.10.2023/Student.cs
@@ -0,0 +1,40 @@
+﻿
+class Student
+{
+    public string Name;
+    public string SurName;
+    public double Group;
+    public int Point;
+    public Boolean isGraduated;
+
+
+    public void GetName(string Name, string SurName)
+    {
+        Console.WriteLine($"Name:{Name}, Surname:{SurName}");
+    }
+
+    public void GetValue(double Group,int Point,Boolean isGraduated)
+    {
+        if ( isGraduated ) 
+        {
+            Console.WriteLine($"Qrup:{Group}, Bal:{Point}, Mezundur");
+        }
+        else
+        {
+            Console.WriteLine($"Qrup:{Group}, Bal:{Point}, Mezun deyildir");
+        }
+    }
+
+    public void EnterExam()
+    {
+        if (Point > 80)
+        {
+            Console.WriteLine("Ikinci imtahana girə bilər :)");
+        }
+        else
+        {
+            Console.WriteLine("Ikinci imtahana kifayət qədər bal toplamamısız :(");
+        }
+    }
+}
+
